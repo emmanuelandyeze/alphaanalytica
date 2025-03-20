f2bb9b06136f27484f8b08ecd0c8018ff396dd86
emmanuelandyeze <emmanuel.andyeze@gmail.com>
2025-03-20 12:56:15 +0100

    update

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Landing() {
 	return (
 		<div className="max-w-[85rem] px-4 md:px-0 mx-auto">
-			<h1 className="md:text-[2.9rem] text-[2.3rem] w-full md:w-[70%] font-[600] md:font-[400] leading-[2.8rem] md:leading-[3.6rem] text-[#0E202A] mt-10">
+			<h1 className="md:text-[2.9rem] text-[2.1rem] w-full md:w-[70%] font-[600] md:font-[400] leading-[2.8rem] md:leading-[3.6rem] text-[#0E202A] mt-10">
 				Innovative Chemical Solutions for a Smarter,
 				Sustainable Future.
 			</h1>
